38b4d71c9141975348e6a42c5fcbde41cb12fa00
user <test@gmail.com>
2021-02-01 11:59:11 +0200

    test

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,9 +3,6 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-  generate: {
-    fallback: true,
-  },
   router: {
     base: '/nuxt-vuetify/',
   },
