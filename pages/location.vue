de659f2f5c1e42581306bd551a28f7c5d38309e1
user <test@gmail.com>
2021-02-01 12:25:25 +0200

    test3

--- pages/location.vue
+++ pages/location.vue
@@ -1,7 +1,6 @@
 <template>
   <v-row>
     <v-col class="text-center">
-      <img src="/v.png" alt="Vuetify.js" class="mb-5" />
       <blockquote class="blockquote">
         &#8220;LOCATION&#8221;
         <footer>
