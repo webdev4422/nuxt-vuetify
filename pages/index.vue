d7fb2a9067a65673de40e2dc1c21196b0fcd5940
user <test@gmail.com>
2021-02-07 13:10:14 +0200

    new feat.

--- pages/index.vue
+++ pages/index.vue
@@ -5,9 +5,7 @@
         <logo />
       </div>
       <v-card>
-        <v-card-title class="headline">
-          TEST DEV BRANCH more changes and more test
-        </v-card-title>
+        <v-card-title class="headline"> testing new feature here </v-card-title>
         <v-card-text>
           <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
           <p>
