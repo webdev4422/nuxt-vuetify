86cd44544f67403a7603351750b51c3432b43d61
user <test@gmail.com>
2021-02-11 15:11:47 +0200

    add app.html

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,16 +20,6 @@ export default {
       { hid: 'description', name: 'description', content: '' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-    script: [
-      {
-        src: 'https://www.googletagmanager.com/gtag/js?id=G-KN81G3C9ZT',
-        async: true,
-      },
-      {
-        src: 'gtag.js',
-        type: 'text/javascript',
-      },
-    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
